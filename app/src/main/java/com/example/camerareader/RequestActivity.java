4cf204b41c21224eaedd92fd2031e2a9631f7b26
SoraSpades <davidandrino2002@gmail.com>
2021-09-29 22:50:52 +0200

    Add generic request to test modules

--- app/src/main/java/com/example/camerareader/RequestActivity.java
+++ app/src/main/java/com/example/camerareader/RequestActivity.java
@@ -3,12 +3,33 @@ package com.example.camerareader;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class RequestActivity extends AppCompatActivity {
+    public final static String urlBase = "";
+
+    private TextView responseView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request);
+
+        responseView = findViewById(R.id.request_activity_body);
+        makeRequest("https://api.coindesk.com/v1/bpi/currentprice.json", Request.Method.GET);
+    }
+
+    private void makeRequest(String url, int method) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(method, url,
+                response -> responseView.setText(response),
+                error -> responseView.setText(R.string.request_error_message));
+
+        queue.add(stringRequest);
     }
 }
