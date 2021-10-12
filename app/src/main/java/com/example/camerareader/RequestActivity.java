86d6250c6975e847ac1f203ab36d7c6cdeb53c6e
SoraSpades <davidandrino2002@gmail.com>
2021-10-12 14:34:13 +0200

    Add String Requests

--- app/src/main/java/com/example/camerareader/RequestActivity.java
+++ app/src/main/java/com/example/camerareader/RequestActivity.java
@@ -17,6 +17,9 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 public class RequestActivity extends AppCompatActivity {
     public final static String URLBASE = "https://codebarreaderserver.herokuapp.com/";
     public final static String QUERYSUFFIX = "query?id=";
@@ -24,48 +27,42 @@ public class RequestActivity extends AppCompatActivity {
 
     private TextView responseView;
     private ImageView imageView;
-    private RequestQueue queue;
+    private Requester requester;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request);
 
+        // Variable Initialization
         responseView = findViewById(R.id.request_activity_body);
         imageView = findViewById(R.id.product_image_view);
         String barcodeData = getIntent().getStringExtra(BARCODE_EXTRA);
-        makeQueryRequest( barcodeData );
+        requester = new Requester(this);
+
+        // Make Request
+        CompletableFuture<String> future = requester.requestString(URLBASE + QUERYSUFFIX + barcodeData);
+        future.thenRun(()->{
+            try {
+                String response = future.get();
+                responseView.setText(response);
+            } catch (Exception e) {
+                Log.d("RESPONSE", "Exception: " + e.toString());
+            }
+        });
     }
 
-    private void makeQueryRequest(String barcode) {
-        queue = Volley.newRequestQueue(this);
-        StringRequest stringRequest = new StringRequest(
-                Request.Method.GET, URLBASE + QUERYSUFFIX + barcode,
-                response -> {// Response callback
-                    responseView.setText(response); // TODO: Format Data Display
-                    try {
-                        Product product = new Product(new JSONObject(response));
-                        makeImageRequest(product.getImgPath());
-                    } catch (JSONException e) {
-                        Log.d("Error parsing JSON", e.toString());
-                    }
-                },
-                error -> responseView.setText(error.toString())); // Error callback
-
-        queue.add(stringRequest);
-    }
-
-    private void makeImageRequest(String image) {
-        ImageRequest imageRequest = new ImageRequest(
-                URLBASE + image,
-                response -> { // Callback
-                    imageView.setImageBitmap(response);
-                },
-                0, 0, // Image dimensions
-                ImageView.ScaleType.CENTER_CROP, // Align type
-                Bitmap.Config.RGB_565, // Color space
-                error -> Log.d("Error in Image Request", error.toString()) // Error callback
-        );
-        queue.add(imageRequest);
-    }
+//    private void makeImageRequest(String image) {
+//        ImageRequest imageRequest = new ImageRequest(
+//                URLBASE + image,
+//                response -> { // Callback
+//                    imageView.setImageBitmap(response);
+//                },
+//                0, 0, // Image dimensions
+//                ImageView.ScaleType.CENTER_CROP, // Align type
+//                Bitmap.Config.RGB_565, // Color space
+//                error -> Log.d("Error in Image Request", error.toString()) // Error callback
+//        );
+//        queue.add(imageRequest);
+//    }
 }
--- app/src/main/java/com/example/camerareader/Requester.java
+++ app/src/main/java/com/example/camerareader/Requester.java
@@ -1,10 +1,13 @@
 package com.example.camerareader;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.util.Log;
+import android.widget.ImageView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -16,26 +19,28 @@ import java.util.concurrent.Future;
 
 public class Requester {
 
-    private RequestQueue queue;
-    private Context context;
+    private final RequestQueue queue;
+    private final Context context;
 
     public Requester(Context context) {
         this.context = context;
         queue = Volley.newRequestQueue(this.context);
     }
 
-    public Future<String> RequestString(String url) {
-        CompletableFuture<String> future = new CompletableFuture<String>();
-        queue = Volley.newRequestQueue(context);
+    public CompletableFuture<String> requestString(String url) {
+        CompletableFuture<String> future = new CompletableFuture<>();
         // Response callback
         StringRequest stringRequest = new StringRequest(
                 Request.Method.GET,
                 url,
-                future::complete, // response -> future.complete(response)
+                response -> {
+                    Log.d("RESPONSE", response);
+                    future.complete(response);
+                },
+//                future::complete, // response -> future.complete(response)
                 error -> future.cancel(true) // Error callback
         );
         queue.add(stringRequest);
         return future;
     }
-
 }
