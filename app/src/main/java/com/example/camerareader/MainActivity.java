c946c07847ea489730cd8d4683433951766449a2
SoraSpades <davidandrino2002@gmail.com>
2021-10-18 17:54:13 +0200

    Remove unused imports

--- app/src/main/java/com/example/camerareader/MainActivity.java
+++ app/src/main/java/com/example/camerareader/MainActivity.java
@@ -6,13 +6,6 @@ import androidx.appcompat.app.AppCompatDelegate;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.vision.CameraSource;
-import com.google.android.gms.vision.barcode.Barcode;
-import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 public class MainActivity extends AppCompatActivity {
 
