08c84a9e95ee41ced0fffce759e480c559bc1d1f
SoraSpades <davidandrino2002@gmail.com>
2021-10-21 17:11:15 +0200

    Implement Insert Code Activity

--- app/src/main/java/com/example/camerareader/insertCode.java
+++ app/src/main/java/com/example/camerareader/insertCode.java
@@ -2,13 +2,34 @@ package com.example.camerareader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class insertCode extends AppCompatActivity {
+
+    EditText codeEditText;
+    Button sendCodeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_code);
+
+        codeEditText = findViewById(R.id.insertCode_EditText);
+        sendCodeButton=findViewById(R.id.insertCode_Button);
+
+        sendCodeButton.setOnClickListener((view) -> {
+            String code = codeEditText.getText().toString();
+            if (!code.equals("")) {
+                Intent intent = new Intent(this, RequestActivity.class);
+                intent.putExtra(RequestActivity.BARCODE_EXTRA, code);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
