4cfd63090907f8a433b34fd6b225b3cbe6afdae2
SoraSpades <davidandrino2002@gmail.com>
2021-10-21 16:50:11 +0200

    Add new Activity to Manually Introduce Codes

---
+++ app/src/main/java/com/example/camerareader/insertCode.java
@@ -0,0 +1,14 @@
+package com.example.camerareader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class insertCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_code);
+    }
+}
