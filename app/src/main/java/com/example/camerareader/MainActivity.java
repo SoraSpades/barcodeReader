a76eb86659564b51e489f3ac132f1231a7d5d014
SoraSpades <davidandrino2002@gmail.com>
2021-11-03 13:00:11 +0100

    Implement ProductList in MainActivity. #1

--- app/src/main/java/com/example/camerareader/MainActivity.java
+++ app/src/main/java/com/example/camerareader/MainActivity.java
@@ -5,15 +5,22 @@ import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private ProductList pList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        pList = new ProductList();
 
         setContentView(R.layout.activity_main);
 
@@ -29,6 +36,24 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.insertCode_Activity_button).setOnClickListener(view->{
             startActivity(new Intent(MainActivity.this, insertCode.class));
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (pList.getLength()>0) {
+            findViewById(R.id.listLayout).setVisibility(View.VISIBLE);
+            String products="", prices="";
+            for (Product p: pList.getList()) {
+                products += p.getName() + '\n';
+                prices += Double.toString(p.getPrice()) + '€' + '\n';
+            }
+            ((TextView)findViewById(R.id.productListNames)).setText(products);
+            ((TextView)findViewById(R.id.productListPrices)).setText(prices);
+        } else {
+            findViewById(R.id.listLayout).setVisibility(View.GONE);
+        }
     }
 
     public void changeTheme(View view) {
